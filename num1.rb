ba8017b64ca221fcd164a96ef6dce7b3f3c1c779
Andrew Brossia <abrossia@gmail.com>
2020-12-21 22:25:02 -0500

    feed the rubocop

--- num1.rb
+++ num1.rb
@@ -1,8 +1,11 @@
-# Insert + or - signs anywhere between the digits 123456789 in such a way that the expression evaluates to 100. The order of the digits must not be changed.
+# frozen_string_literal: true
 
+# Insert + or - signs anywhere between the digits 123456789 in such a way that
+# the expression evaluates to 100. The order of the digits must not be changed.
+#
 # Sample solution: 1 + 2 + 3 - 4 + 5 + 6 + 78 + 9 = 100
-
-# Programming problem: Write a program to output all solutions. 
+#
+# Programming problem: Write a program to output all solutions.
 
 base = (1..9).to_a.join
 
@@ -15,17 +18,16 @@ def evaluate(string)
     result += next_number.to_i if item == '+'
     result -= next_number.to_i if item == '-'
   end
-  return result
+  result
 end
 
 choice_permutations = ['', ' + ', ' - '].repeated_permutation(8).to_a.drop(1)
 
 possible_equations = choice_permutations.map do |choice|
-    base.chars.zip(choice).flatten.compact.join
-  end
+  base.chars.zip(choice).flatten.compact.join
+end
 
 possible_equations.each do |equation|
   result = evaluate(equation)
   puts "#{equation} = 100" if result == 100
 end
-
